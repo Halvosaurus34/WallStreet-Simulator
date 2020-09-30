4d71faccdb94800e76f4aa4d40ff56aa88e2ae8c
Levi Halvorson <lhalvorson34@gmail.com>
2020-09-30 11:53:51 -0600

    changed js to show request with button press

--- develop/app.js
+++ develop/app.js
@@ -1,50 +1,37 @@
 // working api-key, or put your own in here
 var stock = ["GOOGL", "BA", "AXP", "DOW", "HON"];
-var i=0;
-var times = 0
-function getStock() {
-    var settings = {
-      async: true,
-      crossDomain: true,
-      url:
-        "https://alpha-vantage.p.rapidapi.com/query?symbol=" +
-        stock[i] +
-        "&function=GLOBAL_QUOTE",
-      method: "GET",
-      headers: {
-        "x-rapidapi-host": "alpha-vantage.p.rapidapi.com",
-        "x-rapidapi-key": "9778abae07msh6e3fcf350e0115cp17ebcajsn6a16d9555f35",
-      },
-    };
-    $.ajax(settings).done(function (response) {
-      console.log(response);
-      console.log(i);
-      document.querySelector("#list-tab").children[i].innerHTML = response["Global Quote"]["01. symbol"];
-      document.querySelector("#nav-tabContent").children[i].children[1].innerHTML = "Price: " +  response["Global Quote"]["05. price"];
-      i++;
-      }
-    );
-}
+var i = 0;
+var times = 0;
 
-function showinfo(event){
-  console.log("click")
-  for(let i=0; i<5;i++){
-    document.querySelector("#nav-tabContent").children[i].classList.remove("show")
-    document.querySelector("#nav-tabContent").children[i].classList.remove("active")
-  }
-  event.target.classList.add("show");
-  event.target.classList.add("active");
+function getStock(event) {
+  console.log(event.target);
+  var settings = {
+    async: true,
+    crossDomain: true,
+    url:
+      "https://alpha-vantage.p.rapidapi.com/query?symbol=" +
+      event.target.id +
+      "&function=GLOBAL_QUOTE",
+    method: "GET",
+    headers: {
+      "x-rapidapi-host": "alpha-vantage.p.rapidapi.com",
+      "x-rapidapi-key": "9778abae07msh6e3fcf350e0115cp17ebcajsn6a16d9555f35",
+    },
+  };
+  $.ajax(settings).done(function (response) {
+    console.log(response);
+    var symbol = response["Global Quote"]["01. symbol"];
+    var price = response["Global Quote"]["05. price"];
+    var change = response["Global Quote"]["09. change"];
+    $("#nav-tabContent").prepend(`<div
+    class="card mt-2"
+    role="tabpanel"
+    aria-labelledby="list-1-list"
+  >
+    <div>Symbol: ${symbol}</div>
+    <div>Price: $${Number(price).toFixed(2)} US </div>
+    <div id="change">Change: ${Number(change).toFixed(2)}</div>
+  </div>
+`);
+  });
 }
-
-function callfunction(){
-  if (times<stock.length){
-    getStock()
-  }
-  times++;
-}
-document.querySelector("#list-tab").children[0].addEventListener('click',showinfo)
-// for(let i=0; i<5;i++){
-//   document.querySelector("#nav-tabContent").children[i].addEventListener('click',showinfo)
-// }
-
-setInterval(callfunction,1000);
