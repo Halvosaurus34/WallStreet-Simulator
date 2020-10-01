ec79f4cb11ddbc70c53c9769e5dbd4cfb54d161f
Levi Halvorson <lhalvorson34@gmail.com>
2020-09-30 19:32:08 -0600

    changed date format and made links open in new tab

--- develop/news.js
+++ develop/news.js
@@ -17,15 +17,16 @@ function getArticles() {
     console.log(response);
     for (var i = 0; i < responseNum; i++) {
       var date = info[i].pub_date;
-
+      var formatDate = moment(date).format("MMMM Do YYYY, h:mm:ss a");
+      console.log("Date: ", formatDate);
       $("#results")
         .prepend(`<div class="card w-100 mt-3 mb-2" style="width: 18rem;">
       <div class="card-body">
         <h5 class="card-title">${info[i].headline.main}</h5>
         <h6 class="card-subtitle mb-2 text-muted">${info[i].byline.original}</h6>
         <p class="card-text lead">${info[i].section_name}</p>
-        <p class="card-text lead">${date}</p>
-        <a href="${info[i].web_url}" class="card-link lead">${info[i].web_url}</a>
+        <p class="card-text lead">${formatDate}</p>
+        <a href="${info[i].web_url}" class="card-link lead" target="blank">${info[i].web_url}</a>
       </div>
     </div>`);
       console.log(info[i].headline.main);
