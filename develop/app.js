64a1c0266d794b17e28e29709762295a23ab47ed
Levi Halvorson <lhalvorson34@gmail.com>
2020-09-30 18:44:48 -0600

    added search bar js

--- develop/app.js
+++ develop/app.js
@@ -29,15 +29,15 @@ var users = {
 var stock = ["GOOGL", "BA", "AXP", "DOW", "HON"];
 var i = 0;
 var times = 0;
+var query = "";
 
-function getStock(event) {
-  console.log(event.target);
+function getStock() {
   var settings = {
     async: true,
     crossDomain: true,
     url:
       "https://alpha-vantage.p.rapidapi.com/query?symbol=" +
-      event.target.id +
+      query +
       "&function=GLOBAL_QUOTE",
     method: "GET",
     headers: {
@@ -57,12 +57,52 @@ function getStock(event) {
         aria-labelledby="list-1-list"
     >
     <div class="card-title h1">${symbol}</div>
-    <div class="card-text lead">Price: ${price}</div>
-    <div class="card-text lead">Change: ${change}</div>
+    <div class="card-text lead">Price: ${Number(price)}</div>
+    <div class="card-text lead">Change: ${Number(change)}</div>
     </div>
 `);
   });
 }
+
+function getStockBtn(event) {
+  var settings = {
+    async: true,
+    crossDomain: true,
+    url:
+      "https://alpha-vantage.p.rapidapi.com/query?symbol=" +
+      event.target.id +
+      "&function=GLOBAL_QUOTE",
+    method: "GET",
+    headers: {
+      "x-rapidapi-host": "alpha-vantage.p.rapidapi.com",
+      "x-rapidapi-key": "9778abae07msh6e3fcf350e0115cp17ebcajsn6a16d9555f35",
+    },
+  };
+  $.ajax(settings).done(function (response) {
+    console.log(response);
+    var symbol = response["Global Quote"]["01. symbol"];
+    var price = response["Global Quote"]["05. price"];
+    var change = response["Global Quote"]["09. change"];
+    $("#nav-tabContent").html(`<div class=" mt-3 border rounded shadow">
+      <div
+          class="ml-3 mt-3 mb-3"
+          role="tabpanel"
+          aria-labelledby="list-1-list"
+      >
+      <div class="card-title h1">${symbol}</div>
+      <div class="card-text lead">Price: ${Number(price).toFixed(2)}</div>
+      <div class="card-text lead">Change: ${Number(change).toFixed(2)}</div>
+      </div>
+  `);
+  });
+}
+
 for (const property in users) {
   console.log(`${property}: ${users[property]}`);
 }
+
+$("#searchBtn").on("click", function () {
+  console.log("click");
+  query = $("#searchResult").val();
+  getStock();
+});
